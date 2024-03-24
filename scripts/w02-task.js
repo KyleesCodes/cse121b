cd1b848e27d092dea3277474ac20f833c93f8778
KyleeCodes <ktawesome1@gmail.com>
2024-03-23 21:22:49 -0700

    Shorten code

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,27 +7,17 @@ let fullName = `Kylee Tallman`;
 let currentYear = '2024';
 let profilePicture = 'images/HS graduation photo.jpg';
 
-
-
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector("main#home picture img");
 
-
-
-
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 currentYear.textContent = '#year';
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
-
-
-
-
-
 
 /* Step 5 - Array */
 let favoriteFoods = ['Hamburger Rice Casserole', 'Sushi', 'Bulgogi', 'Truffle Mushroom dishes'];
@@ -42,18 +32,3 @@ document.getElementById('food').innerHTML += '<br>' + foodHtml;
 favoriteFoods.pop();
 foodHtml = favoriteFoods.join(",");
 document.getElementById('food').innerHTML += '<br>' + foodHtml;
-
-/*
-let additionalFood = 'Chicken rice salad';
-favoriteFoods.push(additionalFood);
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-favoriteFoods.shift();
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-favoriteFoods.pop();
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-*/
-
-
-
-
-
